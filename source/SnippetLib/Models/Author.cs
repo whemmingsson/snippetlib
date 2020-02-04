e53d4468cc30125e33091e2df193c1bda7109510
William <william.hemmingsson@gmail.com>
2020-02-04 21:14:39 +0100

    Refactoring

--- source/SnippetLib/Models/Author.cs
+++ source/SnippetLib/Models/Author.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SnippetLib.Models
+﻿namespace SnippetLib.Models
 {
     public class Author
     {
--- source/SnippetLib/Models/ErrorViewModel.cs
+++ source/SnippetLib/Models/ErrorViewModel.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace SnippetLib.Models
 {
     public class ErrorViewModel
